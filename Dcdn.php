f734f8f519e591162bc083d262af9decec973714
宋神宗 <zhijiang.nzj@alibaba-inc.com>
2019-05-09 16:23:46 +0800

    Generate composer.json for split (#114)

--- Dcdn.php
+++ Dcdn.php
@@ -5,8 +5,8 @@ namespace AlibabaCloud\Dcdn;
 use AlibabaCloud\Client\Resolver\VersionResolver;
 
 /**
- * @deprecated
+ * @method static V20180115\DcdnApiResolver v20180115()
  */
-class DcdnVersion extends VersionResolver
+class Dcdn extends VersionResolver
 {
 }
